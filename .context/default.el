e6438ad413f563f23bfcb38482936470328e7bf1
Peter Kosov <11111000000@email.com>
2026-04-12 21:42:04 +0800

    Ignore generated artifacts and untrack build outputs

--- .context/default.el
+++
@@ -1,1 +0,0 @@
-(:version 3 :generator "context-navigator/1.1.0" :root "/home/zoya/Code/ProjectsReview" :items nil)
--- dist/chunks/astro_v0G8EdP7.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_BKnmkwkZ.mjs';
-import 'clsx';
